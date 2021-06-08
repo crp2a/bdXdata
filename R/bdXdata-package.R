0f245b61fe6c7f90c2d6310be2d5d78e2c9a5aeb
blebrun <bricelebrun@icloud.com>
2021-06-08 15:22:30 +0200

    Add description, logo and licence.

---
+++ R/bdXdata-package.R
@@ -0,0 +1,34 @@
+#' @details
+#' \tabular{ll}{
+#'  \strong{Package:} \tab bdXdata \cr
+#'  \strong{Type:} \tab Package \cr
+#'  \strong{Version:} \tab 0.1.0 \cr
+#'  \strong{License:} \tab GPL-3
+#' }
+#' @author
+#' \strong{Full list of authors and contributors} (alphabetic order)
+#'
+#' \tabular{ll}{
+#'  Nicolas Frerebeau \tab \emph{Université Bordeaux Montaigne, France} \cr
+#'  Brice Lebrun \tab \emph{Université Bordeaux Montaigne, France} \cr
+#' }
+#'
+#' \strong{Package maintainer}
+#'
+#' Brice Lebrun\cr
+#' \email{brice.lebrun@@u-bordeaux-montaigne.fr}
+#'
+#' IRAMAT-CRP2A (UMR 5060)\cr
+#' Maison de l'Archéologie\cr
+#' Université Bordeaux Montaigne\cr
+#' F-33607 Pessac cedex\cr
+#' France
+#'
+#' @name bdXdata-package
+#' @aliases bdXdata-package bdXdata
+#' @docType package
+#' @keywords internal
+"_PACKAGE"
+
+
+
